dc7701aa65f231ae36df646478546171d700cbd3
Jordi Font <jordifv90@gmail.com>
2022-11-21 18:03:30 +0100

    Add initial server to the project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
+import * as dotenv from "dotenv";
+import express from "express";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+dotenv.config();
 
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+const { log } = console;
+
+app.use((req, res) => {
+  res.status(200).json({ message: "Hola mundo" });
+});
+
+app.listen(port, () => {
+  log(`Server starting: http://localhost:${port}`);
+});
