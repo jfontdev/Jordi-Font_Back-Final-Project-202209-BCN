2a4d8771c17fd023776a82494be97b9f6967b300
Jordi Font <jordifv90@gmail.com>
2022-11-22 09:27:47 +0100

    Add loadEnvironment file that grabs the .env variables

---
+++ src/loadEnvironment.ts
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const environment = {
+  port: process.env.PORT,
+  mongoUrl: process.env.MONGO_URL,
+  secretKey: process.env.SECRET_KEY,
+};
+
+export default environment;
