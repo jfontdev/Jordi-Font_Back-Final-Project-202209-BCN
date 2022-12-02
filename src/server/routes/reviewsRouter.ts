2aae2b0232cec72b4d9f7f86985bed27fdf4b355
Jordi Font <jordifv90@gmail.com>
2022-12-02 09:38:39 +0100

    Add imports extension to reviews controller and router

--- src/server/routes/reviewsRouter.ts
+++ src/server/routes/reviewsRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getReviews } from "../controllers/reviewsController";
+import { getReviews } from "../controllers/reviewsController.js";
 
 const reviewsRouter = express.Router();
 
