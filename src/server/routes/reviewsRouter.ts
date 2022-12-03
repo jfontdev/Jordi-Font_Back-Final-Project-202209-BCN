a96771b148d2ad6770cb86248284105bee7b2ae7
Jordi Font <jordifv90@gmail.com>
2022-12-03 13:18:31 +0100

    Add route for deleteReview controller

--- src/server/routes/reviewsRouter.ts
+++ src/server/routes/reviewsRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getReviews } from "../controllers/reviewsController.js";
+import { deleteReview, getReviews } from "../controllers/reviewsController.js";
 
 const reviewsRouter = express.Router();
 
 reviewsRouter.get("/", getReviews);
+reviewsRouter.delete("/delete/:idReview", deleteReview);
 
 export default reviewsRouter;
