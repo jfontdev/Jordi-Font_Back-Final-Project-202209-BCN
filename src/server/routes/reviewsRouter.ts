0d8a8fcebc900fa00b0d4451fbdb72a7c1ed4d01
jfontvit <jordifv90@gmail.com>
2022-12-04 13:06:08 +0100

    Add createReview endpoint to reviewsRouter

--- src/server/routes/reviewsRouter.ts
+++ src/server/routes/reviewsRouter.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { deleteReview, getReviews } from "../controllers/reviewsController.js";
+
+import {
+  createReview,
+  deleteReview,
+  getReviews,
+} from "../controllers/reviewsController.js";
 
 const reviewsRouter = express.Router();
 
 reviewsRouter.get("/", getReviews);
 reviewsRouter.delete("/delete/:idReview", deleteReview);
+reviewsRouter.post("/create", createReview);
 
 export default reviewsRouter;
